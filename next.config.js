64cb8cff32f6d73a0460c67d3589f42cf0a34d84
choiboy98 <daniel.won.choi@gmail.com>
2024-01-18 01:34:19 -0800

    does this work

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const debug = process.env.NODE_ENV !== "production";
+
 const nextConfig = {
     output: 'export',
+    "/": { page: "/" },
 
-    basePath: '/brb-traveling'
+    assetPrefix: !debug ? 'https://choiboy98.github.io/brb-traveling/' : '',
 }
 
 module.exports = nextConfig
