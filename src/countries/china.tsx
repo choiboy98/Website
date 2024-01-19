15a7b96ffe34e3608f64fee0e203bbb12e143ea1
choiboy98 <daniel.won.choi@gmail.com>
2024-01-19 18:28:20 -0500

    does this video path work

--- src/countries/china.tsx
+++ src/countries/china.tsx
@@ -4,7 +4,7 @@ const Title = "CHINA";
 const CountryCode = "CN";
 const TitleText = "Only talk about the weather and sports!";
 const SubText =
-    "It is quite remarkabe how everything is under one system (definitely scary). Seeing surveillance cameras everywhere you go is an experience. Regardless, what an experience it was. The food, the people, and the view were something else.";
+    "It is quite remarkabe how everything is under one system (definitely scary). Seeing surveillance cameras everywhere you go is an experience. Regardless, what a great time it was. The food, the people, and the view were something else.";
 const textStyles = {
     color: '#A7C7E7'
 };
--- src/countries/finland.tsx
+++ src/countries/finland.tsx
@@ -4,7 +4,7 @@ const Title = "FINLAND";
 const CountryCode = "FI";
 const TitleText = "Finland? More like SkySauna and Santa Claus Village!";
 const SubText =
-    "Finland wasn't on my travel list. But what I saw the SkySauna and the Santa Claus Village? Finland was next on my list! Though the cities weren't too impressive, these two attractions won my heart.";
+    "Finland wasn't on my travel list. But when I saw the SkySauna and the Santa Claus Village? Finland was next on my list! Though the cities weren't too impressive, these two attractions won my heart.";
 const textStyles = {
     color: '#A7C7E7'
 };
